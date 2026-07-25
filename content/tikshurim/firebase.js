e0d0ce61c7f85a602edc0809f8c9f7f31ecf4815
ruthy1954-shiraz <ruthy1954@gmail.com>
2026-07-25 13:56:16 +0300

    Add Firebase configuration and initialization

---
+++ content/tikshurim/firebase.js
@@ -0,0 +1,19 @@
+// firebase.js — הגדרות חיבור ל-Firebase
+
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-app.js";
+import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-analytics.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDdagkGJI-uuz5dbFOelzsUKJL8QpcVgn4",
+  authDomain: "ruthy1954-tikshurim.firebaseapp.com",
+  projectId: "ruthy1954-tikshurim",
+  storageBucket: "ruthy1954-tikshurim.appspot.com",
+  messagingSenderId: "681474709062",
+  appId: "1:681474709062:web:9045fc05c985e81e239c47",
+  measurementId: "G-EF3S4JJ8SP"
+};
+
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
+export { app };
